07c940ff3dd3647362017be90effe63ed0a17e62
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-07-19 00:05:21 -0400

    feat: ✨ determine if RoboGlance api is healthy based on tba status
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -1,13 +1,37 @@
+import os
 from fastapi import FastAPI
 from pydantic import BaseModel
+from httpx import AsyncClient
 
 app = FastAPI()
 
 
-class HealthResponse(BaseModel):
+class RoboGlanceStatus(BaseModel):
     healthy: bool
+    the_blue_alliance_healthy: bool
 
 
-@app.get("/health-check")
-def read_health() -> HealthResponse:
-    return HealthResponse(healthy=True)
+tba_api_key = os.getenv("THE_BLUE_ALLIANCE_API_KEY")
+
+
+@app.get("/status")
+async def read_status() -> RoboGlanceStatus:
+    tba_healthy = await is_tba_healthy(tba_api_key)
+
+    results = RoboGlanceStatus(
+        healthy=tba_healthy,
+        the_blue_alliance_healthy=tba_healthy,
+    )
+    return results
+
+
+async def is_tba_healthy(api_key: str | None) -> bool:
+    if api_key is None:
+        return False
+
+    async with AsyncClient() as client:
+        response = await client.get(
+            "https://www.thebluealliance.com/api/v3/status",
+            headers={"X-TBA-Auth-Key": api_key},
+        )
+        return response.is_success
