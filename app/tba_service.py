609efb62b0b80e8cc530fd436ea400f1505f7c70
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-07-20 14:18:11 -0400

    refactor: 🥅 improve separation btwn tba service/settings
    Enables throwing a 500 error when tba api key is missing only once by giving it a direct entry point as an argument to the TbaService class. It is guaranteed to exist (not be None) after this point. Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- app/tba_service.py
+++ app/tba_service.py
@@ -2,13 +2,13 @@ from httpx import AsyncClient
 
 
 class TbaService:
-    async def is_tba_healthy(api_key: str | None) -> bool:
-        if api_key is None:
-            return False
+    def __init__(self, api_key: str):
+        self.api_key = api_key
 
+    async def is_tba_healthy(self) -> bool:
         async with AsyncClient(
             base_url="https://www.thebluealliance.com/api/v3",
-            headers={"X-TBA-Auth-Key": api_key},
+            headers={"X-TBA-Auth-Key": self.api_key},
         ) as tba_client:
             response = await tba_client.get("/status")
             return response.is_success
