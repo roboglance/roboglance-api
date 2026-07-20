49f6990cd12820cbf3bf1abb59c632ac79071220
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-07-19 22:23:21 -0400

    refactor: ♻️ create class to act as api client for tba
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -3,8 +3,8 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
-from httpx import AsyncClient
 
+from .tba_client import TbaClient
 from .settings import RoboGlanceSettings
 
 app = FastAPI()
@@ -24,22 +24,10 @@ class RoboGlanceStatus(BaseModel):
 async def read_status(
     settings: Annotated[RoboGlanceSettings, Depends(get_settings)],
 ) -> RoboGlanceStatus:
-    tba_healthy = await is_tba_healthy(settings.tba_api_key)
+    tba_healthy = await TbaClient.is_tba_healthy(settings.tba_api_key)
 
     results = RoboGlanceStatus(
         healthy=tba_healthy,
         the_blue_alliance_healthy=tba_healthy,
     )
     return results
-
-
-async def is_tba_healthy(api_key: str | None) -> bool:
-    if api_key is None:
-        return False
-
-    async with AsyncClient() as client:
-        response = await client.get(
-            "https://www.thebluealliance.com/api/v3/status",
-            headers={"X-TBA-Auth-Key": api_key},
-        )
-        return response.is_success
