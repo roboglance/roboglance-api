8ae51fcafd73c420770f7475a53407ddf1ae4ed8
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-07-22 11:19:00 -0400

    refactor: ♻️ move AsyncClient out of TbaService to enable testing
    Needs to be an argument of TbaService so that we can use dependency injection to replace it with a mock in testing. Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- app/tba_service.py
+++ app/tba_service.py
@@ -2,13 +2,9 @@ from httpx import AsyncClient
 
 
 class TbaService:
-    def __init__(self, api_key: str):
-        self.api_key = api_key
+    def __init__(self, tba_client: AsyncClient):
+        self.tba_client = tba_client
 
     async def is_tba_healthy(self) -> bool:
-        async with AsyncClient(
-            base_url="https://www.thebluealliance.com/api/v3",
-            headers={"X-TBA-Auth-Key": self.api_key},
-        ) as tba_client:
-            response = await tba_client.get("/status")
-            return response.is_success
+        response = await self.tba_client.get("/status")
+        return response.is_success
