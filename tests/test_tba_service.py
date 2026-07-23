f5a44832c24c7c384907d77dd72b29fc0f5b691e
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-07-23 14:37:41 -0400

    test: ✅ make tba_service tests use real client with mocked transport
    https://www.b-list.org/weblog/2023/dec/08/mock-python-httpx/ . Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- tests/test_tba_service.py
+++ tests/test_tba_service.py
@@ -1,30 +1,30 @@
-from httpx import AsyncClient, Response
-from pytest_mock import MockerFixture
+from httpx import AsyncClient, MockTransport
+from respx import Router
 
 from app.tba_service import TbaService
 
 
-async def test_is_tba_healthy_should_be_true_if_response_is_success(
-    mocker: MockerFixture,
-):
-    mock_tba_client = mocker.create_autospec(AsyncClient, spec_set=True)
-    mock_response = mocker.create_autospec(Response, spec_set=True)
-    mock_tba_client.get.return_value = mock_response
-    mock_response.is_success = True
+async def test_is_tba_healthy_should_be_true_if_response_is_success():
+    router = Router(base_url="https://example.com/api")
+    router.get("/status").respond(200)
 
-    tba_service = TbaService(mock_tba_client)
-    assert await tba_service.is_tba_healthy() is True
-    mock_tba_client.get.assert_called_once_with("/status")
+    async with AsyncClient(
+        base_url="https://example.com/api",
+        transport=MockTransport(router.async_handler),
+    ) as mock_tba_client:
+        tba_service = TbaService(mock_tba_client)
+        assert await tba_service.is_tba_healthy() is True
+        router.assert_all_called()
 
 
-async def test_is_tba_healthy_should_be_false_if_response_is_not_success(
-    mocker: MockerFixture,
-):
-    mock_tba_client = mocker.create_autospec(AsyncClient, spec_set=True)
-    mock_response = mocker.create_autospec(Response, spec_set=True)
-    mock_tba_client.get.return_value = mock_response
-    mock_response.is_success = False
+async def test_is_tba_healthy_should_be_false_if_response_is_not_success():
+    router = Router(base_url="https://example.com/api")
+    router.get("/status").respond(500)
 
-    tba_service = TbaService(mock_tba_client)
-    assert await tba_service.is_tba_healthy() is False
-    mock_tba_client.get.assert_called_once_with("/status")
+    async with AsyncClient(
+        base_url="https://example.com/api",
+        transport=MockTransport(router.async_handler),
+    ) as mock_tba_client:
+        tba_service = TbaService(mock_tba_client)
+        assert await tba_service.is_tba_healthy() is False
+        router.assert_all_called()
