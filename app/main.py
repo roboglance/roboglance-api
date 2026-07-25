30966b2d0a0b3689f98f1d0839ef52f26cb8b6da
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-07-25 12:34:23 -0400

    refactor: ♻️ pull out client logic from get_tba_service()
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def get_settings():
     return RoboGlanceSettings()
 
 
-async def get_tba_service(
+async def get_tba_client(
     settings: Annotated[RoboGlanceSettings, Depends(get_settings)],
 ):
     if settings.tba_api_key is None:
@@ -29,7 +29,7 @@ async def get_tba_service(
         base_url="https://www.thebluealliance.com/api/v3",
         headers={"X-TBA-Auth-Key": settings.tba_api_key},
     ) as tba_client:
-        yield TbaService(tba_client)
+        yield tba_client
 
 
 class RoboGlanceStatus(BaseModel):
@@ -39,9 +39,8 @@ class RoboGlanceStatus(BaseModel):
 
 @app.get("/status")
 async def read_status(
-    tba_service: Annotated[TbaService, Depends(get_tba_service)],
+    tba_service: Annotated[TbaService, Depends(TbaService)],
 ) -> RoboGlanceStatus:
-
     tba_healthy = await tba_service.is_tba_healthy()
 
     results = RoboGlanceStatus(
--- app/tba_service.py
+++ app/tba_service.py
@@ -1,10 +1,15 @@
+from typing import Annotated
+
+from fastapi import Depends
 from httpx import AsyncClient
+
+from app.main import get_tba_client
 
 
 class TbaService:
     _tba_client: AsyncClient
 
-    def __init__(self, tba_client: AsyncClient):
+    def __init__(self, tba_client: Annotated[AsyncClient, Depends(get_tba_client)]):
         self._tba_client = tba_client
 
     async def is_tba_healthy(self) -> bool:
