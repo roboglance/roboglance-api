fe7b3ba1b317b1edd6b1d5803174e4f952ac16f0
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-07-25 15:45:06 -0400

    test: ✅ write tests for status endpoint
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

---
+++ tests/test_main.py
@@ -0,0 +1,51 @@
+from typing import Any, Callable
+
+from fastapi.testclient import TestClient
+from pytest import fixture
+from pytest_mock import MockFixture, MockType
+
+
+from app.main import app
+from app.dependencies.tba_service import TbaService
+
+
+@fixture
+def dependency_overrides():
+    yield app.dependency_overrides
+    app.dependency_overrides = {}
+
+
+DependencyOverrides = dict[Callable[..., Any], Callable[..., MockType]]
+
+
+@fixture
+def mock_tba_service(mocker: MockFixture, dependency_overrides: DependencyOverrides):
+    mock = mocker.create_autospec(TbaService, spec_set=True)
+    dependency_overrides[TbaService] = lambda: mock
+    return mock
+
+
+@fixture
+def test_client():
+    with TestClient(app) as client:
+        yield client
+
+
+async def test_status_should_be_healthy_when_tba_is_healthy(
+    test_client: TestClient,
+    mock_tba_service: MockType,
+):
+    mock_tba_service.is_tba_healthy.return_value = True
+    response = test_client.get("/status")
+    assert response.status_code == 200
+    assert response.json() == {"healthy": True, "the_blue_alliance_healthy": True}
+
+
+async def test_status_should_not_be_healthy_when_tba_is_not_healthy(
+    test_client: TestClient,
+    mock_tba_service: MockType,
+):
+    mock_tba_service.is_tba_healthy.return_value = False
+    response = test_client.get("/status")
+    assert response.status_code == 200
+    assert response.json() == {"healthy": False, "the_blue_alliance_healthy": False}
