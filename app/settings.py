11a1cf441c32c5ad11bc6798eb20e28aa86b9888
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2026-07-24 22:32:14 -0400

    fix: 🐛 read env file when not debugging in VSCode

--- app/settings.py
+++ app/settings.py
@@ -4,4 +4,4 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class RoboGlanceSettings(BaseSettings):
     tba_api_key: str | None = None
 
-    model_config = SettingsConfigDict(env_prefix="RoboGlance_")
+    model_config = SettingsConfigDict(env_prefix="RoboGlance_", env_file=".env")
