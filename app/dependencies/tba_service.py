aed9a8163a4f1f4d13b9c5ee8023d7ec62fb591f
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2026-08-30 21:19:41 -0400

    feat: ✨ add function to get tba team
    Co-authored-by: Nate Stringham <nstringham@users.noreply.github.com>

--- app/dependencies/tba_service.py
+++ app/dependencies/tba_service.py
@@ -1,9 +1,25 @@
+from http import HTTPStatus
 from typing import Annotated
 
 from fastapi import Depends
 from httpx import AsyncClient
+from pydantic import BaseModel
 
 from app.dependencies.tba_client import TbaClientDependency
+
+
+class TbaTeam(BaseModel):
+    team_number: int
+    nickname: str
+    name: str
+    school_name: str | None
+    city: str | None
+    state_prov: str | None
+    country: str | None
+
+
+class NonExistentTbaTeamError(Exception):
+    pass
 
 
 class TbaService:
@@ -15,6 +31,13 @@ class TbaService:
     async def is_tba_healthy(self) -> bool:
         response = await self._tba_client.get("/status")
         return response.is_success
+
+    async def get_team(self, team_number: int) -> TbaTeam:
+        response = await self._tba_client.get(f"/team/frc{team_number}")
+        if response.status_code == HTTPStatus.NOT_FOUND:
+            raise NonExistentTbaTeamError
+        response.raise_for_status()
+        return TbaTeam.model_validate(response.json())
 
 
 type TbaServiceDependency = Annotated[TbaService, Depends(TbaService)]
